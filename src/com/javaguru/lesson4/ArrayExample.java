bde2739dbf12df79ac4560db32a21bbddbbb93c5
java2017autumn@gmail.com <java2017autumn@gmail.com>
2017-10-21 19:45:58 +0300

    lesson 4

--- src/com/javaguru/lesson4/ArrayExample.java
+++ src/com/javaguru/lesson4/ArrayExample.java
@@ -1,4 +1,17 @@
 package com.javaguru.lesson4;
 
+import java.util.Random;
+
 public class ArrayExample {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+    }
+
+    public static int getRandomNumber(){
+        Random random = new Random();
+        return random.nextInt(101);
+    }
+
 }

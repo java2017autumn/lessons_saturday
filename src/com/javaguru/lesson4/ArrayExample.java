afd94271fc2dcab9b58ba285a93fdde39c59d331
java2017autumn@gmail.com <java2017autumn@gmail.com>
2017-10-21 20:02:27 +0300

    lesson 4

--- src/com/javaguru/lesson4/ArrayExample.java
+++ src/com/javaguru/lesson4/ArrayExample.java
@@ -5,11 +5,28 @@ import java.util.Random;
 public class ArrayExample {
 
     public static void main(String[] args) {
-        int[] array = new int[10];
-
+        int[] array = createArray(10);
+        fillArrayWithRandomNumbers(array);
+        printArray(array);
     }
 
-    public static int getRandomNumber(){
+    public static void printArray(int[] array) {
+        for (int number : array) {
+            System.out.println(number);
+        }
+    }
+
+    public static int[] createArray(int length) {
+        return new int[length];
+    }
+
+    public static void fillArrayWithRandomNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = getRandomNumber();
+        }
+    }
+
+    public static int getRandomNumber() {
         Random random = new Random();
         return random.nextInt(101);
     }

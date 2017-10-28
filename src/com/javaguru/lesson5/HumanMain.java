687c75836f59e63891fab445bab58ad40331ea8b
java2017autumn@gmail.com <java2017autumn@gmail.com>
2017-10-28 19:34:18 +0300

    lesson 5

---
+++ src/com/javaguru/lesson5/HumanMain.java
@@ -0,0 +1,12 @@
+package com.javaguru.lesson5;
+
+
+public class HumanMain {
+
+    public static void main(String[] args) {
+        Human human = new Human();
+        human.setName("Vasya");
+        human.setAge(15);
+        human.sayHello();
+    }
+}
